5b8be92100275263cb67d7c6839992bc59f0ab16
“abdanrafii” <abdanrafii8@gmail.com>
2024-07-01 23:15:10 +0700

    kipas

--- resources/views/themes/indotoko/home.blade.php
+++ resources/views/themes/indotoko/home.blade.php
@@ -33,7 +33,7 @@
       </div>
       <div class="col-lg-3 col-6">
         <div class="card card-body p-lg-4 p3">
-          <a href="#"><img src="{{ asset('themes/indotoko/assets/img/shop_02.jpg') }}" alt="" class="img-fluid"></a>
+          <a href="#"><img src="{{ asset('/img/kipas2.jpg') }}" alt="" class="img-fluid"></a>
           <h3 class="product-name mt-3">Product 2</h3>
           {{-- <div class="rating">
             <i class="bx bxs-star"></i>
